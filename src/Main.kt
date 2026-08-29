ff7a63b289006402424696dd9629e0d2a6bee9d8
kamauma <kamaumary0406@gmail.com>
2026-08-29 21:48:35 +0300

    mary

--- src/Main.kt
+++ src/Main.kt
@@ -5,7 +5,7 @@ fun main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
-    println("Hello World")
+    println("Hellow World")
     println("-------------------------------------------")
     println("Welcome to our new Tenant Management System")
     println("-------------------------------------------")
