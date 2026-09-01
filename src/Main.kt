75949046f3242392b3668c3a71b90679eed117ec
kostakis2000 <kostakis.oladaro@strathmore.edu>
2026-09-01 20:14:19 +0300

    initial commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello " + name + "!")
-    println("Hello  World")
-    println("-------------------------------------------")
-    println("Welcome to our new Tenant Management System")
-    println("-------------------------------------------")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
 }
-//commit
+
 
 
